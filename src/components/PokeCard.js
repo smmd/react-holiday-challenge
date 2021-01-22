c7193491f416052d6f309316300ccfe418da71a8
smeneses <smeneses@etsy.com>
2021-01-22 00:48:53 -0600

    feat: list my favourite pokemones

---
+++ src/components/PokeCard.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { mockPokemonData } from '../mock/pokeData';
+
+class Pokemon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.pokeInfo = mockPokemonData;
+  }
+
+  render() {
+    return (
+        <div>
+          {this.pokeInfo.pokemones.map((pokemon) => {
+            return (
+                <PokemonInfo pokemon={pokemon} />
+            )
+          })}
+        </div>
+    );
+  }
+}
+
+function PokemonInfo(props) {
+  return (
+      <div className='Pokemon'>
+        <h3>{props.pokemon.name}</h3>
+        <img src={props.pokemon.sprites.front_default}/>
+        <a href={props.pokemon.video} target="_blank">Video</a>
+      </div>
+  )
+}
+
+export default Pokemon;
